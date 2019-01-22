99132c92ea5c92dbd068c18a3f03fdcd198724af
Guillaume lale NEBIE <gnebie@student.42.fr>
2019-01-22 21:13:57 +0100

    [ADD] recuperation des categories

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import re
+import xml.etree.ElementTree as etree
 
 def mounthrepl(matchobj):
 	if matchobj.group(2) == 'janvier':
@@ -30,13 +31,76 @@ def mounthrepl(matchobj):
 	return ("<date>" + matchobj.group(3) + "0" + matchobj.group(1) + "</date>")
 
 def replace_line(line):
-	newline = re.sub(r'\[\[(.*?)\]\]', r'<geo>\1</geo>', line)
+	newline = re.sub(r'\[\[(([A-Z]|jardin|grottes|parc|chutes).*?)\]\]', r'<geo>\1</geo>', line)
 	newline2 = re.sub(r'\{\{date\|([0-9]{1,2})\|(janvier|février|mars|avril|mai|juin|juillet|août|septembre|octobre|novembre|décembre)\|([0-9]{4})\}\}', mounthrepl, newline)
 	return newline2
 
+def grep_categories(text):
+	categories = re.findall(r'\[\[Catégorie:.*?\]\]', text)
+	cleancategorie = []
+	for categorie in categories:
+		cleancategorie.append(re.sub(r'\[\[Catégorie:(.*?)(\| |\|\*)?\]\]', r'\1', categorie))
+	text = re.sub(r'\[\[Catégorie:.*?\]\]', ' ', text)
+	return cleancategorie, text
+
+
+def strip_tag_name(t):
+    idx = k = t.rfind("}")
+    if idx != -1:
+        t = t[idx + 1:]
+    return t
 
 def parse_file(file_name):
 	try:
+		print("test")
+		i = 0
+		for event, elem in etree.iterparse(file_name, events=('start', 'end')):
+			i += 1
+			tname = strip_tag_name(elem.tag)
+
+			if event == 'start':
+				if tname == 'page':
+					title = ''
+					# id = -1
+					# redirect = ''
+					# inrevision = False
+					# ns = 0
+				elif tname == 'revision':
+					# Do not pick up on revision id's
+					inrevision = True
+			else:
+				a = 0;
+				# print(tname)
+				if tname == 'title':
+					# print("title " + elem.text)
+					title = elem.text
+				elif tname == 'text':
+					categorie, text = grep_categories(elem.text)
+					print(categorie)
+					text = replace_line(text)
+					# print(text)
+			if (i == 1000):
+				return
+				# elif tname == 'id' and not inrevision:
+				# 	id = int(elem.text)
+				# elif tname == 'redirect':
+				# 	redirect = elem.attrib['title']
+				# elif tname == 'ns':
+				# 	ns = int(elem.text)
+				# elif tname == 'page':
+				# 	print("page " + elem.text)
+				# 	totalCount += 1
+				# 	if ns == 10:
+				# 		templateCount += 1
+				# 		templateWriter.writerow([id, title])
+				# 	elif len(redirect) > 0:
+				# 		articleCount += 1
+				# 		articlesWriter.writerow([id, title, redirect])
+				# 	else:
+				# 		redirectCount += 1
+				# 		redirectWriter.writerow([id, title, redirect])
+
+
 		i = 0
 		with open(file_name, 'r') as f:
 			for line in f:
@@ -50,14 +114,20 @@ def parse_file(file_name):
 	except IOError:
 		print("Error when file tryed to be open")
 		exit(0)
-	except:
-		print("Error when file tryed to be open")
-		exit(0)
+	# except:
+	# 	print("Error when file tryed to be open")
+	# 	exit(0)
 
 def main():
 	try:
-		parse_file("/Users/gnebie/goinfre/download/frwiki-latest-pages-articles1.xml-p3p275787")
-	except:
+		parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+		parser.add_argument("file_name", help="File path")
+		args = parser.parse_args()
+
+		parse_file(args.file_name)
+		# "/Users/gnebie/goinfre/download/frwiki-latest-pages-articles1.xml-p3p275787"
+	# except:
+	except IOError:
 		print("Unknow Error catch")
 
 if __name__ == '__main__':
