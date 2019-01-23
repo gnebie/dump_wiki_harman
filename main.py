86a99608bcef0fa90bc56dca29fe312f34fe4a35
Guillaume lale NEBIE <gnebie@student.42.fr>
2019-01-23 04:18:07 +0100

    [ADD] Sleep not found so it's time for coding ! article parsing

--- main.py
+++ main.py
@@ -30,12 +30,40 @@ def mounthrepl(matchobj):
 		return ("<date>" + matchobj.group(3) + "12" + matchobj.group(1) + "</date>")
 	return ("<date>" + matchobj.group(3) + "0" + matchobj.group(1) + "</date>")
 
+# fonction de l'enfer #perf not found
 def replace_line(line):
-	newline = re.sub(r'\[\[(([A-Z]|jardin|grottes|parc|chutes).*?)\]\]', r'<geo>\1</geo>', line)
-	newline2 = re.sub(r'\{\{date\|([0-9]{1,2})\|(janvier|février|mars|avril|mai|juin|juillet|août|septembre|octobre|novembre|décembre)\|([0-9]{4})\}\}', mounthrepl, newline)
-	return newline2
+	# suppression de tout ce qu'il y a apres Notes et références
+	cut_index = line.find("== Notes et références ==")
+	if (cut_index is not -1):
+		line = line[0:cut_index]
+	cut_index = line.find("==Notes et références==")
+	if (cut_index is not -1):
+		line = line[0:cut_index]
+	# suppression du complement d'information des blocs [[]]
+	while re.search(r'\[\[([^\]]*?)\[\[(.*?)\]\]([^\]]*?)\]\]', line):
+		line = re.sub(r'\[\[([^\]]*?)\[\[(.*?)\]\]([^\]]*?)\]\]', r'[[\1\2\3]]', line) # pas opti du tout
+	line = re.sub(r'\[\[(Fichier:.*?)\]\]', '', line)
+	while re.search(r'\[\[([^\]]*?)\|([^\]]*?)\]\]', line) is not None:
+		line = re.sub(r'\[\[([^\]]*?)\|([^\]]*?)\]\]', r'[[\2]]', line) # pas opti du tout
+	# Traitement géographique : ils correspondes a des valeurs entre [[]] commenssant par jardin|grottes|parc|chutes ou une majuscule
+	# Permet une présélection en enlevant une partie des données n'etant pas géographiques mais
+	# Très limité car tous les noms propres sont vu comme des sites géographiques
+	line = re.sub(r'\[\[(([A-Z]|jardin|grottes|parc|chutes|col |île).*?)\]\]', r'<geo>\1</geo>', line)
+	#Récupération de la date au format {{date|00|mois|0000}}
+	#ne prends pas les dates de naissance et de déces
+	line = re.sub(r'\{\{date\|([0-9]{1,2})\|(janvier|février|mars|avril|mai|juin|juillet|août|septembre|octobre|novembre|décembre)\|([0-9]{4})\}\}', mounthrepl, line)
+	# traitement des éléments de de la page wikipedia pour les transformer en phrases
+	line = re.sub(r'\[\[(.*?)\]\]', r'\1', line)
+	line = re.sub(r'({{Article détaillé\||{{Ref|{{ref)[^{]*?}}', '', line)
+	line = re.sub(r'<ref>.*?</ref>', '', line)
+	while re.search(r'{{([^{]*?)(\|)([^{]*?)}}', line):
+		line = re.sub(r'{{([^{]*?)(\|)([^{]*?)}}', r'{{\1 \3}}', line)
+	line = re.sub(r'{{([^{]*?)}}', r'\1', line)
+	line = line.replace('\n\n', '\n')
+	return line
 
 def grep_categories(text):
+	# recuperation des categories et suppression des valeurs inutiles
 	categories = re.findall(r'\[\[Catégorie:.*?\]\]', text)
 	cleancategorie = []
 	for categorie in categories:
@@ -50,51 +78,48 @@ def strip_tag_name(t):
         t = t[idx + 1:]
     return t
 
+"utf-8"
+
+def write_file(categorie, text, title):
+	# codecs.open(pathArticles, "w", ENCODING) as articlesFH
+	categorie = etree.Element("categorie")
+	article = etree.SubElement(categorie, "article")
+	article_title = etree.SubElement(article, "title").text = title
+	article_text = etree.SubElement(article, "text").text = text
+	tree = etree.ElementTree(categorie)
+	etree.dump(tree)
+	# print(etree.tostring(tree))
+	print("\n\n\n\n")
+
 def parse_file(file_name):
 	try:
+		title = ''
+		text = ''
 		print("test")
 		i = 0
 		for event, elem in etree.iterparse(file_name, events=('start', 'end')):
-			i += 1
 			tname = strip_tag_name(elem.tag)
 			if event == 'start':
 				if tname == 'page':
 					title = ''
-					# id = -1
-					# redirect = ''
-					# inrevision = False
-					# ns = 0
-				elif tname == 'revision':
-					# Do not pick up on revision id's
-					inrevision = True
+					text = ''
 			else:
 				a = 0;
 				# print(tname)
 				if tname == 'title':
-					# print("title " + elem.text)
 					title = elem.text
 				elif tname == 'text':
+					i += 1
 					categorie, text = grep_categories(elem.text)
-					print(categorie)
+					# print(categorie)
 					text = replace_line(text)
-					print(text)
-
-		# i = 0
-		# with open(file_name, 'r') as f:
-		# 	for line in f:
-		# 		print (i)
-		# 		newline = replace_line(line)
-		# 		print(newline)
-		# 		i += 1
-		# 		if (i == 5890):
-		# 			break ;
-		# f.close();
+					# print(text)
+					write_file(categorie, text, title)
+					if i == 2:
+						return ;
 	except IOError:
 		print("Error when file tryed to be open")
 		exit(0)
-	# except:
-	# 	print("Error when file tryed to be open")
-	# 	exit(0)
 
 def main():
 	try:
